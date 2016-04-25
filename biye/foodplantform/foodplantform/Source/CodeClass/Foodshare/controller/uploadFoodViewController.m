5f9ba7405745653e5deccdf6d9e25e7e8097e03d
MWHHud <953976025@qq.com>
2016-04-25 12:01:05 +0800

    food

--- biye/foodplantform/foodplantform/Source/CodeClass/Foodshare/controller/uploadFoodViewController.m
+++ biye/foodplantform/foodplantform/Source/CodeClass/Foodshare/controller/uploadFoodViewController.m
@@ -10,6 +10,8 @@
 #import "loadupFoodView.h"
 @interface uploadFoodViewController ()
 @property(nonatomic,strong)loadupFoodView *lv;
+@property(nonatomic,assign)BOOL isFullScreen;
+@property(nonatomic,assign)BOOL isupload;
 @end
 
 @implementation uploadFoodViewController
@@ -21,9 +23,227 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.lv.imgBtn addTarget:self action:@selector(imgBtnAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.lv.chooseRec addTarget:self action:@selector(chooseRecAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.lv.chooseSty addTarget:self action:@selector(chooseStyAction) forControlEvents:UIControlEventTouchUpInside];
     // Do any additional setup after loading the view.
 }
 
+#pragma ====选择照片====
+// 选取照片
+-(void)imgBtnAction
+{
+    UIActionSheet *sheet;
+    
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+        
+    {
+        sheet  = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"拍照",@"从相册选择", nil];
+        
+    }
+    
+    else {
+        
+        sheet = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"从相册选择", nil];
+        
+    }
+    
+    
+    sheet.tag = 255;
+    
+    [sheet showInView:self.view];
+    
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (actionSheet.tag == 255) {
+        
+        NSUInteger sourceType = 0;
+        
+        // 判断是否支持相机
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            
+            switch (buttonIndex) {
+                case 0:
+                    // 取消
+                    return;
+                case 1:
+                    // 相机
+                    sourceType = UIImagePickerControllerSourceTypeCamera;
+                    break;
+                    
+                case 2:
+                    // 相册
+                    sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                    break;
+            }
+        }
+        else {
+            if (buttonIndex == 0) {
+                
+                return;
+            } else {
+                sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+            }
+        }
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        
+        imagePickerController.delegate = self;
+        
+        imagePickerController.allowsEditing = YES;
+        
+        imagePickerController.sourceType = sourceType;
+        
+        [self presentViewController:imagePickerController animated:YES completion:^{}];
+        
+        
+    }
+    
+    if (actionSheet.tag == 256) {
+        if (buttonIndex == 0) {
+            return;
+        }
+        if (buttonIndex == 1) {
+            self.lv.rec.text = @"接受预订";
+        }
+        else
+        {
+            self.lv.rec.text = @"不接受预订";
+        }
+    }
+    
+    if (actionSheet.tag == 257) {
+        if (buttonIndex == 0) {
+            return;
+        }
+        if (buttonIndex == 1) {
+            self.lv.sty.text = @"堂食";
+        }
+        else
+        {
+            self.lv.rec.text = @"外卖";
+        }
+    }
+
+
+}
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+    
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    NSDate *  senddate=[NSDate date];
+    
+    NSDateFormatter  *dateformatter=[[NSDateFormatter alloc] init];
+    
+    [dateformatter setDateFormat:@"YYYYMMddHHmmss"];
+    
+    NSString *  locationString=[dateformatter stringFromDate:senddate];
+    
+    [self saveImage:image withName:[NSString stringWithFormat:@"%@.png",locationString]];
+    
+    NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+    
+    NSString *fullPath = [cachesPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.png",locationString]];
+    
+    UIImage *savedImage = [[UIImage alloc] initWithContentsOfFile:fullPath];
+    
+    
+    _isFullScreen = NO;
+    
+    [self.lv.picture setImage:savedImage];
+    
+    self.lv.picture.tag = 100;
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+NSData * UIImageJPEGRepresentation ( UIImage *image, CGFloat compressionQuality);
+- (void) saveImage:(UIImage *)currentImage withName:(NSString *)imageName
+{
+    
+    NSData *imageData = UIImageJPEGRepresentation(currentImage, 0.5);
+    // 获取沙盒目录
+    
+    NSString *cachesPath = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+    
+    NSString *fullPath = [cachesPath stringByAppendingPathComponent:imageName];
+    
+    
+    // 将图片写入文件
+    
+    [imageData writeToFile:fullPath atomically:NO];
+}
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    _isFullScreen = !_isFullScreen;
+    UITouch *touch = [touches anyObject];
+    
+    CGPoint touchPoint = [touch locationInView:self.view];
+    
+    CGPoint imagePoint = self.lv.picture.frame.origin;
+    //touchPoint.x ，touchPoint.y 就是触点的坐标
+    
+    // 触点在imageView内，点击imageView时 放大,再次点击时缩小
+    if(imagePoint.x <= touchPoint.x && imagePoint.x +self.lv.picture .frame.size.width >=touchPoint.x && imagePoint.y <=  touchPoint.y && imagePoint.y+self.lv.picture.frame.size.height >= touchPoint.y)
+    {
+        // 设置图片放大动画
+        [UIView beginAnimations:nil context:nil];
+        // 动画时间
+        [UIView setAnimationDuration:1];
+        
+        if (_isFullScreen) {
+            // 放大尺寸
+            
+            self.lv.picture.frame = CGRectMake(0, 0, 320, 480);
+        }
+        else {
+            // 缩小尺寸
+            self.lv.picture.frame = CGRectMake(50, 65, 90, 115);
+        }
+        
+        // commit动画
+        [UIView commitAnimations];
+        
+    }
+    
+}
+
+#pragma ====选择是否接受订单====
+-(void)chooseRecAction
+{
+    UIActionSheet *sheet;
+    
+   
+        sheet  = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"接受预订",@"不接受预订", nil];
+        
+   
+    
+    sheet.tag = 256;
+    
+    [sheet showInView:self.view];
+
+}
+
+
+
+#pragma ====选择类型====
+-(void)chooseStyAction
+{
+    UIActionSheet *sheet;
+    
+    
+    sheet  = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"堂食",@"外卖", nil];
+    
+    
+    
+    sheet.tag = 257;
+    
+    [sheet showInView:self.view];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
--- biye/foodplantform/foodplantform/Source/CodeClass/Foodshare/view/loadupFoodView.m
+++ biye/foodplantform/foodplantform/Source/CodeClass/Foodshare/view/loadupFoodView.m
@@ -20,10 +20,52 @@
 
 -(void)p_setView
 {
+    // 美食名称
     self.foodName = [[UITextField alloc]initWithFrame:CGRectMake(0, 80, kScreenWidth, 50)];
     self.foodName.placeholder = @"美食名称";
     self.foodName.textAlignment = UITextAlignmentCenter;
     [self addSubview:_foodName];
+    
+    // 美食描述
+    self.foodDes = [[UITextField alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.foodName.frame)+10, kScreenWidth, 100)];
+    self.foodDes.placeholder = @"美食描述";
+    self.foodDes.textAlignment = UITextAlignmentCenter;
+    [self addSubview:_foodDes];
+    
+    // 详细地址
+    self.address = [[UITextField alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.foodDes.frame)+10, kScreenWidth, 50)];
+    self.address.placeholder = @"详细地址";
+    self.address.textAlignment = UITextAlignmentCenter;
+    [self addSubview:_address];
+    
+    // 各种BUtton
+    self.imgBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.imgBtn.frame = CGRectMake(0, CGRectGetMaxY(self.address.frame)+10, kScreenWidth/3.0, 30);
+    [self.imgBtn setTitle:@"选择图片" forState:UIControlStateNormal];
+    [self addSubview:_imgBtn];
+    
+    self.chooseRec = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.chooseRec.frame = CGRectMake(CGRectGetMaxX(self.imgBtn.frame), CGRectGetMinY(self.imgBtn.frame), kScreenWidth/3.0, 30);
+    [self.chooseRec setTitle:@"是否预订" forState:UIControlStateNormal];
+    [self addSubview:_chooseRec];
+    
+    
+    self.chooseSty = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.chooseSty.frame = CGRectMake(CGRectGetMaxX(self.chooseRec.frame), CGRectGetMinY(self.imgBtn.frame), kScreenWidth/3.0, 30);
+    [self.chooseSty setTitle:@"选择类型" forState:UIControlStateNormal];
+    [self addSubview:_chooseSty];
+    
+    // 结果
+    
+    self.picture = [[UIImageView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.imgBtn.frame)+10, 100, 100)];
+    [self addSubview:_picture];
+    
+    self.rec = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(self.picture.frame)+10, CGRectGetMinY(self.picture.frame), kScreenWidth-110, 50)];
+    [self addSubview:_rec];
+    
+    self.sty = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMinX(self.rec.frame), CGRectGetMaxY(self.rec.frame), CGRectGetWidth(self.rec.frame), 50)];
+    [self addSubview:_sty];
+    
 }
 /*
 // Only override drawRect: if you perform custom drawing.
