ee6e6b5b32dbee8bb8fbfbcabb7b68f89cd0877d
MWHHud <953976025@qq.com>
2016-04-27 23:38:44 +0800

    reg

---
+++ biye/foodplantform/foodplantform/Source/BaseClass/regAndLogTool/regAndLogTool.h
@@ -0,0 +1,13 @@
+//
+//  regAndLogTool.h
+//  foodplantform
+//
+//  Created by 马文豪 on 16/4/27.
+//  Copyright © 2016年 马文豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface regAndLogTool : NSObject
+
+@end
---
+++ biye/foodplantform/foodplantform/Source/BaseClass/regAndLogTool/regAndLogTool.m
@@ -0,0 +1,13 @@
+//
+//  regAndLogTool.m
+//  foodplantform
+//
+//  Created by 马文豪 on 16/4/27.
+//  Copyright © 2016年 马文豪. All rights reserved.
+//
+
+#import "regAndLogTool.h"
+
+@implementation regAndLogTool
+
+@end
