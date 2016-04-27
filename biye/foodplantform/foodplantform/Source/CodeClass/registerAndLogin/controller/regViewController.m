7b0a382d49b724d82733f3784595e4a4e74f5247
MWHHud <953976025@qq.com>
2016-04-27 22:06:46 +0800

    register BUG

--- biye/foodplantform/foodplantform/Source/CodeClass/registerAndLogin/controller/regViewController.m
+++ biye/foodplantform/foodplantform/Source/CodeClass/registerAndLogin/controller/regViewController.m
@@ -39,7 +39,36 @@
 //注册按钮点击事件
 -(void)regAction:(id) sender
 {
+    if ([self.rv.userName.text  isEqual:@""] || [self.rv.pwStr.text  isEqual: @""] || [self.rv.ensurePwStr.text isEqualToString:@""]) {
+        UIAlertView * mes = [[UIAlertView alloc]initWithTitle:@"提示" message:@"注册内容不能为空" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [mes show];
+    }
     
+    else
+    {
+        if (![self.rv.pwStr.text isEqualToString:self.rv.ensurePwStr.text]) {
+            UIAlertView * mes1 = [[UIAlertView alloc]initWithTitle:@"提示" message:@"两次密码输入不一致" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+            [mes1 show];
+        }
+        else
+        {
+            // GET请求  同步
+            // 1. 准备URL
+            NSString *urlStr = [NSString stringWithFormat:@"http://zhaohm.com.cn/hm/hmWeb/server/mwh/mwhregister.php?username=%@&pwd=%@",self.rv.userName.text,self.rv.pwStr.text];
+            NSURL *url = [NSURL URLWithString:urlStr];
+            // 2. 准备请求
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+            [request setHTTPMethod:@"GET"];
+            // 3. 准备数据
+            NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+            // 4. 解析
+            NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+            
+            NSLog(@"%@",dict);
+        }
+    }
+  
+
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
