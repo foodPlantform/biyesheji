c9fc9c7f166466ff8471306f970cf99acaa87266
斌斌斌 <1272917376@qq.com>
2016-05-11 14:16:58 +0800

    集成环信SDK

---
+++ biye/foodplantform/foodplantform/Source/CodeClass/order/Model/UserApplyListModel.h
@@ -0,0 +1,13 @@
+//
+//  UserApplyListModel.h
+//  foodplantform
+//
+//  Created by 斌斌斌 on 16/5/11.
+//  Copyright © 2016年 马文豪. All rights reserved.
+//
+
+#import "BmobOrderModel.h"
+
+@interface UserApplyListModel : BmobOrderModel
+
+@end
